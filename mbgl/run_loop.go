0084ee4c7b207b30208258c94b3d8f9fee1e68a2
Chris Daley <chebizarro@gmail.com>
2018-06-12 14:14:36 -0700

    changed method receivers to pointers

--- mbgl/run_loop.go
+++ mbgl/run_loop.go
@@ -9,14 +9,15 @@ type RunLoop struct {
 	cptr uintptr
 }
 
-func (r RunLoop) cPtr() uintptr {
+func (r *RunLoop) cPtr() uintptr {
 	return r.cptr
 }
 
-func (r RunLoop) Destroy() {
+func (r *RunLoop) Destroy() {
 	C.mbgl_run_loop_destroy(C.MbglRunLoop(r.cptr))
 }
 
-func NewRunLoop() RunLoop {
-	return RunLoop{ uintptr(C.mbgl_run_loop_new()) }
+func NewRunLoop() *RunLoop {
+	rl := RunLoop{ uintptr(C.mbgl_run_loop_new()) }
+	return &rl
 }
