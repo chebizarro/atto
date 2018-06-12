02bd443f57dc1dafee50f02f26b6e54bf86eaee0
Chris Daley <chebizarro@gmail.com>
2018-06-12 14:11:22 -0700

    changed method receivers to pointers

--- mbgl/lat_lng.go
+++ mbgl/lat_lng.go
@@ -9,14 +9,15 @@ type LatLng struct {
 	cptr uintptr
 }
 
-func (l LatLng) cPtr() uintptr {
+func (l *LatLng) cPtr() uintptr {
 	return l.cptr
 }
 
-func NewLatLng(lat, lon float64) LatLng {
-	return LatLng { uintptr(C.mbgl_lat_lng_new(C.double(lat), C.double(lon))) }
+func NewLatLng(lat, lon float64) *LatLng {
+	lt := LatLng { uintptr(C.mbgl_lat_lng_new(C.double(lat), C.double(lon))) }
+	return &lt
 }
 
-func (l LatLng) Destroy() {
+func (l *LatLng) Destroy() {
 	C.mbgl_lat_lng_destroy(C.MbglLatLng(l.cptr))
 }
